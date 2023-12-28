e00b37f918d576498ae36afba1e4ef5e08ae4cb6
noaha90 <noahabb@outlook.com>
2023-12-27 20:19:00 -0500

    h
    e

--- vite-project/js/array.js
+++ vite-project/js/array.js
@@ -1,7 +1,36 @@
 const array = [
-    {
-    u: `https://pokeapi.co/api/v2/pokemon/azurill`,
-      text: "This Pokemon's Has Evolved: ",
+  {
+    u: `https://pokeapi.co/api/v2/pokemon/`,
+    text: "A Move This Pokemon Can Learn Is:",
+    route: ["random","move","name"],
+    rand: "moves",
+    back: "",
+  },
+  {
+    u: `https://pokeapi.co/api/v2/pokemon/`,
+    text: "This Pokemon's Height Is ",
+    route: ["height"],
+    back: "",
+  },
+  {
+    u: `https://pokeapi.co/api/v2/pokemon/`,
+    text: "This Pokemon's Weight Is ",
+    route: ["weight"],
+    back: "",
+  },
+  {
+    u: `https://pokeapi.co/api/v2/pokemon-species/`,
+    text: "This Pokemon's Capture Rate Is: ",
+    route: ["capture_rate"],
+    back: "",
+  },
+  {
+    u: `https://pokeapi.co/api/v2/pokemon/`,
+    text: "This Pokemon Can Be Found In: ",
+    route: ["random","location_area","name"],
+    rand: "start",
+    alttext: "This Pokemon Cannot Be Found In The Wild!",
+    back: "/encounters",
   },
   ]
 
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,20 +1,74 @@
 import {array} from "./array";
 
 
-async function test(input){
+async function test(choice,guess){
+  console.log(input)
   try{
-  const response = await fetch(input)
+  const response = await fetch(choice.u + input + choice.back)
   const data = await response.json(); 
-  console.log(data)
-}
+  const response2 = await fetch(`https://pokeapi.co/api/v2/pokemon/` + input)
+  const data2 = await response2.json(); 
+  if(guess == data2.name){
+    newGame("Win: ",data.name,data.id)
+  }
+  else if(lives == 0){
+    newGame("Lose: ",data2.name,data.id)
+  }
+  else{
+    if(data == ""){
+      insert(choice.alttext,"")
+    }
+    else{
+    lastJuan(data,choice,0)
+  }
+}}
 catch(error){
   console.log(error)
 }
 }
 
 
-test(array[0].u)
+function lastJuan(start,choice,p){ 
+  if(choice.route[p] != undefined){
+    if(choice.route[p] == "random"){
+      if(choice.rand == "start"){
+        console.log(Object.values(start)[Math.floor(Math.random() * Object.keys(start).length)])
+        lastJuan(Object.values(start)[Math.floor(Math.random() * Object.keys(start).length)],choice,p+1)
+      }
+      Object.keys(start).forEach(key =>{
+        if(key == choice.rand){
+          console.log(key)
+          let steve = (Object.values(start)[Object.keys(start).indexOf(choice.rand)])
+          lastJuan(steve[Math.floor(Math.random() * Object.keys(steve).length)],choice,p+1)
+        }
+      })
+    }
+  Object.keys(start).forEach(key => {
+    if(key.includes(choice.route[p])){
+      let next = Object.values(start)[Object.keys(start).indexOf(key)]
+      lastJuan(next,choice,p+1)}})}
+else{
+  insert(choice.text,start)
+}
+}
 
+
+
+
+function insert(text,va,data){
+let comb = text + va
+if(usedHints.includes(comb) == false){
+document.getElementById("hints").insertAdjacentHTML("beforeend", `<h1 class="hinttext">${comb}</h1>`)
+lives--
+document.getElementsByClassName(".livetext").innerHTML
+document.getElementById("lives").textContent = lives + 1
+usedHints.push(comb)
+}
+else{
+test(array[Math.floor(Math.random()* array.length)],document.getElementById("value").value)
+}
+}
+        
 
 
 const DOMSelectors = {
@@ -29,6 +83,7 @@ const DOMSelectors = {
 stuff.style.display = "none";
 let html = DOMSelectors.every.innerHTML
 let completeList = false
+
 
 function restart(text,pokeName,id){
   usedHints = []
@@ -49,17 +104,18 @@ document.getElementById("start").addEventListener("click", function(event) {
   DOMSelectors.every.insertAdjacentHTML("beforeend", html)
   dropSer() 
   DOMSelectors.gal.innerHTML = ""
-    getData(input)
   stuff.style.display = "block";
+  test(array[Math.floor(Math.random()* array.length)])
+  console.log("e")
   document.getElementById("button2").addEventListener("click", function(event) {
     event.preventDefault()
-    console.log(document.getElementById("dropdown").value)
-    getData(input,document.getElementById("value").value)
+    console.log("unova")
+    test(array[Math.floor(Math.random()* array.length)],document.getElementById("value").value)
 })
 })
 }
 
-
+//Math.floor(Math.random() * 648) + 1
 let input = Math.floor(Math.random() * 648) + 1
 let lives = 6
 let usedHints = []
@@ -67,176 +123,15 @@ let usedHints = []
 
 
 
-async function getData(input,guess){
-  let URL = `https://pokeapi.co/api/v2/pokemon/${input}`
-  let areas = `https://pokeapi.co/api/v2/pokemon/${input}/encounters`
-  let details = `https://pokeapi.co/api/v2/pokemon-species/${input}`
-  let evo = `https://pokeapi.co/api/v2/pokemon-species/${input}`
-  try {
-      const response = await fetch(URL)
-      const data = await response.json(); 
-      const response2 = await fetch(areas)
-      const data2 = await response2.json();
-      const response3 = await fetch(details)
-      const data3 = await response3.json();
-      let total = 0
-      
-      cool(data,data2,data3,total,guess)
-  } catch (error) { 
-    console.log(error)
-
-        }
- 
-}
-
-function cool(data,data2,data3,total,guess){
-    data.stats.forEach(stat =>{
-        total = total + stat.base_stat
-      })
-      console.log(guess)
-      let choices = [
-        {
-          data: data3.evolves_from_species != null,
-          text: "This Pokemon's Has Evolved: ",
-      },
-          {
-              data: data3.shape.name,
-              text: "This Pokemon's Shape Is: ",
-          },
-          {
-            data: data.types,
-            text: "One Possible Type Of This Pokemon is ",
-            route: ["type","name"],
-        },
-          {
-            data: data3.capture_rate,
-            text: "This Pokemon Has A Capture Rate Of: ",
-        },
-          {
-            data: total,
-            text: "This Pokemon Has A Base Stat Total Of ",
-        },
-      {
-        data: data3.flavor_text_entries,
-        text: "Entry: ",
-        route: ["flavor_text"]
-    },
-          {
-            data: data3.color.name,
-            text: "This Pokemon's Color Is ",
-        },
-          {
-            data: data2,
-            text: "One Route This Pokemon Can Be Found In Is: ",
-            route: ["location_area","name"],
-            alt: "This Pokemon Cannot Be Found In The Wild",
-        },
-          {
-              data: data.weight/10,   
-              text: "The Weight of This Pokemon, in Kilograms, is ",
-          },
-          {
-              data: data.abilities,
-              text: "One Possible Ability Of This Pokemon is ",
-              route: ["ability","name"],  
-          },
-          {
-              data: data.moves,
-              text: "One Possible Move Of This Pokemon is ",
-              route: ["move","name"],
-          },
-          {
-              data: data.height/10,
-              text: "The Height of This Pokemon, in Meters, is ",
-          },
-          {
-              data: data.held_items,
-              text: "This Pokemon Can Be Found In The Wild Holding ",
-              route: ["item","name"],
-              alt: "This Pokemon Cannot Hold A Held Item In The Wild",
-          },
-          {
-              data: data.id,
-              text: "The Pokemon's National Dex Number is ",
-          },
-      ]
-      console.log(data.name)
-      if(guess === data.name){
-        newGame("You Won! The Pokemon Is",data)
-      }
-  else if(lives < 0){
-    newGame("You Lost :(. The Pokemon Was",data)
-  }
-  else{
-    let pick = choices[Math.floor(Math.random() * choices.length)]
-    if(typeof pick.data == "number"|| typeof pick.data == "string" || typeof pick.data == "boolean" || pick.data == "true"){
-      console.log(pick.data)  
-      insert(pick.text,pick.data,data)
-    }
-    else if(pick.data == ""){
-      insert(pick.alt,"",data)
-    }
-    else{
-    let ran = Math.floor(Math.random() * Object.keys(pick.data).length)
-    console.log(pick.data[ran])
-    lastOne(pick.data[ran],pick,0,data)
-  }
-}
-}
-
-function newGame(text,data){
-  let complete = text + " " + data.name
-  stuff.style.display = "none";
-  DOMSelectors.every.innerHTML = ""
-  document.getElementById("lives").innerHTML = ""
-  document.getElementById("hints").innerHTML = ""
-  document.getElementById("every").insertAdjacentHTML("beforeend", `<h1>${text} ${data.name}</h1> `) 
-  input = Math.floor(Math.random()* 648) + 1
-  if(data.name == "porygon-z"||data.name == "mr-mime"){
-    let pokeName = data.name
-    restart(complete,pokeName,data.id)
-  }
-  else{
-    let pokeName = data.name.split('-')
-    restart(complete,pokeName,data.id)
-  // document.getElementById("gal").insertAdjacentHTML("beforeend", `<a href="https://pokemondb.net/pokedex/${pokeName}"><span class="imgbor"><img class="img" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png"></img><img class="img" src="https://www.serebii.net/sunmoon/pokemon/${data.id}.png</img></a></span>`)
-  }
-}
 
 
 
-function lastOne(start,pick,fi,data){
-    Object.keys(start).forEach(key => {
-      if(key.includes(pick.route[fi])){
-        let i = Object.keys(start).indexOf(key)
-        let next = Object.values(start)[i]
-        if(typeof next == "object"){
-          lastOne(next,pick,fi+1,data)
-        }
-        else{
-          insert(pick.text,next,data)
-        }}});}
 
-
-function insert(text,va,data){
-  let comb = text + va
-  if(usedHints.includes(comb) == false){
-  document.getElementById("hints").insertAdjacentHTML("beforeend", `<h1 class="hinttext">${comb}</h1>`)
- lives--
- document.getElementsByClassName(".livetext").innerHTML
- document.getElementById("lives").textContent = lives + 2
- usedHints.push(comb)
-}
-else{
-  getData(input)
-}
-}
 
 
 let pokemonList = []
 
  async function collect(){
-  console.log("epic")
   try {
     for(let f=1;f<650;f++){
       let URL = `https://pokeapi.co/api/v2/pokemon/${f}`
@@ -248,7 +143,7 @@ let pokemonList = []
       dropSer()
     }
     catch (error) { 
-      console.log("eaeaea")
+      console.log("error")
           }}
 
           collect()
@@ -274,6 +169,23 @@ console.log(pokemonList)
 
 
 
+function newGame(text,name,id){
+  console.log(name)
+  let complete = text + " " + name
+  stuff.style.display = "none";
+  DOMSelectors.every.innerHTML = ""
+  document.getElementById("lives").innerHTML = ""
+  document.getElementById("hints").innerHTML = ""
+  document.getElementById("every").insertAdjacentHTML("beforeend", `<h1>${text} ${name}</h1> `) 
+  input = Math.floor(Math.random()* 648) + 1
+  if(name == "porygon-z"||name == "mr-mime"){
+    let pokeName = name
+    restart(complete,name,id)
+  }
+  else{
+    restart(complete,name,id)
+  }
+}
 
 
 
@@ -281,3 +193,4 @@ console.log(pokemonList)
 
 
 restart("Press The Button Below To Start!","azumaril",null)
+
