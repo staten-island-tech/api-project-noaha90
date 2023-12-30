ecc995686b0970b7b5af0cf11439282659d18600
noaha90 <noahabb@outlook.com>
2023-12-30 15:01:25 -0500

    going back
    WHY DONT WE TAKE THIS CODE, AND PUSH IT SOMEWHERE ELSE

--- vite-project/js/doms.js
+++ vite-project/js/doms.js
@@ -3,8 +3,10 @@ const DOMSelectors = {
     button2: document.getElementById("button2"),
     form: document.getElementById("value"),
     gal: document.getElementById("gal"),
+    lives: document.getElementById("lives"),
     hints: document.getElementById("hints"),
     every: document.getElementById("every"),
+    silo: document.getElementById("silo"),
   }
 
 export {DOMSelectors}
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,75 +1,97 @@
 import {array} from "./array";
 import { DOMSelectors } from "./doms";
 
+//Math.floor(Math.random() * 648) + 1
 
-let wins = 0
+let input = "joltik"
+let lives = 6
+let usedHints = []
+let guessList = []
+let wins = 0 
 let losses = 0
+let games = 0
 
 async function test(choice,guess){
   console.log(choice)
-  console.log(input)
   try{
   const response = await fetch(choice.u + input + choice.back)
   const data = await response.json(); 
   const response2 = await fetch(`https://pokeapi.co/api/v2/pokemon/` + input)
   const data2 = await response2.json(); 
   if(guess == data2.name){
-    newGame("Win: ",data.name,data.id)
     wins++
+    games++
+    newGame("Win: ",data2.name,data2.id)
+    return wins
   }
   else if(lives == 0){
-    newGame("Lose: ",data2.name,data.id)
-    losses++
+    games++
+    newGame("Lose: ",data2.name,data2.id)
   }
   else{
-    if(data == ""){
-      insert(choice.alttext,"","")
+    if(guessList.includes(guess) == false){
+    if(guess == "porygon-z"||guess == "mr-mime"){
+      DOMSelectors.silo.insertAdjacentHTML("beforeend",`<img class="guess" src="https://play.pokemonshowdown.com/sprites/gen5ani/${guess.replaceAll("-","")}.gif"></img>`)
     }
     else{
-    lastJuan(data,choice,0)
+      DOMSelectors.silo.insertAdjacentHTML("beforeend",`<img class="guess" src="https://play.pokemonshowdown.com/sprites/gen5ani/${guess.split("-")[0] }.gif"></img>`)
+    }
   }
+  guessList.push(guess)
+  lastJuan(data,choice,0)
 }}
 catch(error){
   console.log(error)
-}
-}
+}}
 
 
 function lastJuan(start,choice,p){ 
-  console.log(start)
+  if(start == false){
+    test(array[Math.floor(Math.random()* array.length)],document.getElementById("value").value)
+    return start
+  }
+  if(start == "" || start == undefined || start == true){
+    insert(choice.alttext,"","")
+    return start
+  }
   if(choice.route[p] != undefined){
     if(choice.route[p] == "random"){
       if(choice.rand == "start"){
-        console.log(Object.values(start)[Math.floor(Math.random() * Object.keys(start).length)])
         lastJuan(Object.values(start)[Math.floor(Math.random() * Object.keys(start).length)],choice,p+1)
       }
+      else{
       Object.keys(start).forEach(key =>{
         if(key == choice.rand){
-          console.log(key)
           let steve = (Object.values(start)[Object.keys(start).indexOf(choice.rand)])
           lastJuan(steve[Math.floor(Math.random() * Object.keys(steve).length)],choice,p+1)
-        }
-      })
-    }
+        }})}}
   Object.keys(start).forEach(key => {
     if(key.includes(choice.route[p])){
       let next = Object.values(start)[Object.keys(start).indexOf(key)]
       lastJuan(next,choice,p+1)}})}
 else{
+  if(choice.unitc == true){
+    insert(choice.text,start/10,choice.tend)
+  }
+  else{
   insert(choice.text,start,choice.tend)
-}
-}
+}}}
 
 
 
 
 function insert(text,va,end){
+  console.log(va)
+let ratio = "Winrate: " + Math.round(wins/games*10000)/100 + "%"
+if(ratio == "Winrate: NaN%"){
+  ratio = "No Games Played"
+}
 let comb = text + va + end
 if(usedHints.includes(comb) == false){
 document.getElementById("hints").insertAdjacentHTML("beforeend", `<h1 class="hinttext">${comb.replaceAll('-', ' ').toUpperCase()}</h1>`)
 lives--
-document.getElementsByClassName(".livetext").innerHTML
-document.getElementById("lives").textContent = lives + 1
+clear([DOMSelectors.lives])
+document.getElementById("lives").insertAdjacentHTML("beforeend", `<p id="livecount">Lives: ${lives+1}</p><p>Wins: ${wins}</p><p>Losses: ${games-wins}</p><p>Games: ${games}</p><p>${ratio}</p>`)
 usedHints.push(comb)
 }
 else{
@@ -80,15 +102,13 @@ test(array[Math.floor(Math.random()* array.length)],document.getElementById("val
 
 stuff.style.display = "none";
 let html = DOMSelectors.every.innerHTML
-let completeList = false
 
 function restart(text,pokeName,id){
-  usedHints = []
+  usedHints = guessList = []
   lives = 6
-  // check if remove hints does anything?
-  clear([every,gal,hints])
+  clear([every,gal,hints,silo])
 if(id != null){
-DOMSelectors.gal.insertAdjacentHTML("beforeend", `<a href="https://pokemondb.net/pokedex/${pokeName}"><img class="img" id="${pokeName}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png"></img></a>`)}
+DOMSelectors.gal.insertAdjacentHTML("beforeend", `<a href="https://pokemondb.net/pokedex/${pokeName}"><img class="img" id="${pokeName}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png"></img></a>`)}
 DOMSelectors.every.insertAdjacentHTML("beforeend", `
   <h1>${text} </h1>
   <form class="cool">
@@ -102,17 +122,19 @@ document.getElementById("start").addEventListener("click", function(event) {
   }
   clear([gal])
   stuff.style.display = "block";
-  test(array[Math.floor(Math.random()* array.length)])
+  entry()
   document.getElementById("button2").addEventListener("click", function(event) {
     event.preventDefault()
+    //guessList.includes(document.getElementById("value").value) == false && document.getElementById("value").value != ""
+    if(12!=1){
     test(array[Math.floor(Math.random()* array.length)],document.getElementById("value").value)
+  }
+  else{
+    console.log("Already Guessed!")
+  }
 })
 })
 }
-
-let input = Math.floor(Math.random() * 648) + 1
-let lives = 6
-let usedHints = []
 
 
 function clear(list){
@@ -121,7 +143,16 @@ function clear(list){
   })
 }
 
-
+async function entry(){
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon-species/` + input)
+  const data = await response.json(); 
+  let i = 0
+  while(data.flavor_text_entries[i].language.name != "en"){
+    i++
+  }
+  let censored = data.flavor_text_entries[i].flavor_text.toUpperCase().replaceAll(data.name.toUpperCase(),"XXXX").replaceAll(""," ")
+  insert("Pokedex Entry: ", censored,"")
+}
 
 
 let pokemonList = []
@@ -158,18 +189,16 @@ async function dropSer(){
 
 
 function newGame(text,name,id){
-  console.log(name)
   let complete = text + " " + name
   stuff.style.display = "none";
   clear([every])
   document.getElementById("every").insertAdjacentHTML("beforeend", `<h1>${text} ${name}</h1> `) 
   input = Math.floor(Math.random()* 648) + 1
   if(name == "porygon-z"||name == "mr-mime"){
-    let pokeName = name
     restart(complete,name,id)
   }
   else{
-    restart(complete,name,id)
+    restart(complete,name.split("-")[0],id)
   }
 }
 
